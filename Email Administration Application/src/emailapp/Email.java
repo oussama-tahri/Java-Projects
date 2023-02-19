aa1b6b0b1aa02c28f5c881e70614430abd15c3b3
Oussama Tahri <odmf77@gmail.com>
2023-02-19 02:00:03 +0100

    Update Email.java

--- Email Administration Application/src/emailapp/Email.java
+++ Email Administration Application/src/emailapp/Email.java
@@ -5,22 +5,7 @@ import java.util.Scanner;
 
 public class Email {
 
-
-      /*                              -- Email Application --
-
-            Scenario: You are an IT Support Administrator Specialist and are
-                        charged with the task of creating email accounts for new hires.
-
-            This application would do the following :
-                1- Generate an email with the following syntax: firstname.lastname@deparment.company.com
-                2- Determinate the department (sales, development, accounting), if none leave blank.
-                3- Generate a random String for password.
-                4- Have set methods to change the password, set the mailbox capacity and define and alternate
-                    email address.
-                5- Have get methods to display the name, email and mailbox capacity
-    * */
-
-
+      
     private String firstName;
     private String lastName;
     private String email;
